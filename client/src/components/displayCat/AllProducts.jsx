79ec1c750fce49ba459df7b34910b2243affb7c4
devyanit05 <thokaldevyani123@gmail.com>
2022-11-27 00:26:01 +0530

    responsive

--- client/src/components/displayCat/AllProducts.jsx
+++ client/src/components/displayCat/AllProducts.jsx
@@ -4,32 +4,29 @@ import Title from "./CatTitle";
 import ProductCard from "./ProductCard";
 import { connect } from "react-redux";
 
-
 const AllProducts = (props) => {
   return (
     <div className="main_container">
       <div className="page_title">
         <Title title="All Products" />
       </div>
-      <div className="all_cards">
-      { props.products && props.products.products.map((product) => {
-        return(
-          <div className="single_card">
-            <ProductCard product = {product}/>
-          </div>
-        )
-      })
-      }
+      <div className="all_cards all_cards ml-md-5 d-flex align-content-center flex-wrap">
+        {props.products &&
+          props.products.products.map((product) => {
+            return (
+              <div className="single_card mx-4">
+                <ProductCard product={product} />
+              </div>
+            );
+          })}
       </div>
     </div>
   );
 };
 const mapStateToProps = (state) => {
-  return{
-    products:state.products
-  }
-}
-
-
+  return {
+    products: state.products,
+  };
+};
 
 export default connect(mapStateToProps)(AllProducts);
