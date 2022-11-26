8ecbfee38c2246ca9187a0f28f031a9fb3d1b8f0
Ashhar909 <ashhar374@gmail.com>
2022-11-26 22:29:53 +0530

    setting up card

--- client/src/components/displayCat/AllProducts.jsx
+++ client/src/components/displayCat/AllProducts.jsx
@@ -2,20 +2,34 @@ import React from "react";
 import "./display.css";
 import Title from "./CatTitle";
 import ProductCard from "./ProductCard";
+import { connect } from "react-redux";
 
-const AllProducts = () => {
+
+const AllProducts = (props) => {
   return (
     <div className="main_container">
       <div className="page_title">
         <Title title="All Products" />
       </div>
       <div className="all_cards">
-        <div className="single_card">
-          <ProductCard />
-        </div>
+      { props.products && props.products.products.map((product) => {
+        return(
+          <div className="single_card">
+            <ProductCard product = {product}/>
+          </div>
+        )
+      })
+      }
       </div>
     </div>
   );
 };
+const mapStateToProps = (state) => {
+  return{
+    products:state.products
+  }
+}
 
-export default AllProducts;
+
+
+export default connect(mapStateToProps)(AllProducts);
