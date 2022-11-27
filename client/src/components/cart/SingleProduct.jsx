f479fcb0bb9ad8096c7ccd401f667eca936dfba4
devyanit05 <thokaldevyani123@gmail.com>
2022-11-27 18:59:11 +0530

    msg

--- client/src/components/cart/SingleProduct.jsx
+++ client/src/components/cart/SingleProduct.jsx
@@ -1,34 +1,47 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import "./cart.css";
 import { Divider } from "@mui/material";
-import { letterSpacing } from "@mui/system";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 
 const Cart = (props) => {
+<<<<<<< HEAD
+=======
+  const params = useParams()
+  const id = params.id.toString()
+  let product = props.products.products.filter((product) => {
+    return product._id === id
+  })
+  useEffect(() => {
+    // console.log(id)
+    // console.log(product)
+    // eslint-disable-next-line
+  }, [])
+  
+>>>>>>> 15d8d1041e2241bb1a74eb39462ad23b89d89279
   return (
     <div className="cart_section">
       <div className="cart_container">
         <div className="left_cart">
-          <img src="" alt="cartimg" />
+          <img src={require(`../../../../images/${product[0].product_img}`)} alt="cartimg" />
           <div className="cart_btn">
             <button className="cart_btn1">Add to Cart</button>
             <button className="cart_btn2">Buy Now</button>
           </div>
         </div>
         <div className="right_cart">
-          {/* <h3>{productData.product_name}</h3>
-          <h4>{productData.product_category}</h4>
+          <h3>{product[0].product_name}</h3>
+          <h4>{product[0].product_category}</h4>
           <Divider />
-          <p className="mrp">{productData.product_mrp}</p> */}
+          <p className="mrp">Cost: Rs.{product[0].product_mrp}</p>
           <p>
-            Owner : <span style={{ color: "#b12704" }}>Devyaniii</span>
+            Owner : <span style={{ color: "#b12704" }}>{product[0].owner_name}</span>
           </p>
-          <p>Owner email : devyani.thokal@gmail.com</p>
+          <p>Owner email : {product[0].owner_email}</p>
 
           <div className="discount_box">
             <h5>
-              Discount : <span style={{ color: "#111" }}>47%</span>
+              Discount : <span style={{ color: "#111" }}>{product[0].product_discount}%</span>
             </h5>
           </div>
           <p className="description">
@@ -41,14 +54,7 @@ const Cart = (props) => {
                 letterSpacing: "0.4px",
               }}
             >
-              lorem30 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Quaerat, quo maxime, illo rem enim debitis non pariatur odio nam
-              dicta aliquid laborum saepe consequuntur nesciunt doloremque quis?
-              Amet, corrupti voluptatibus iste, veritatis pariatur tempora non
-              asperiores voluptatem cumque soluta magnam voluptates tempore
-              fAnimi evditiis fugit maxime fugiat repellat. Totam ullam
-              reprehenderit quae perspiciatis, ducimus rerum. Facere quod et
-              nisi deserunt.
+              {product[0].product_description}
             </span>
           </p>
         </div>
@@ -57,4 +63,10 @@ const Cart = (props) => {
   );
 };
 
-export default Cart;
+const mapStateToProps = (state) => {
+  return{
+    products : state.products
+  }
+}
+
+export default connect(mapStateToProps)(Cart);
